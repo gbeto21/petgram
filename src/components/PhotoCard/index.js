eb911030b349c2a502b5855aa2650b735a410664
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-21 14:20:40 -0600

    Se cambio la funcionalidad de los Hocks a sus respectivos archivos. Se creo la carpeta hooks con los hooks usados en el proyecto. Se termino la clase: 'Custom Hooks: usenearScreen y useLocalStorage'.

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,52 +1,17 @@
-/* eslint-disable space-before-blocks */
-import React, { useEffect, useRef, useState, Fragment } from 'react'
+
+import React, { Fragment } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
+  const [show, element] = useNearScreen()
   const key = `like-${id}`
-  const [liked, setLiked] = useState(() => {
-    try {
-      const like = window.localStorage.getItem(key)
-      return like
-    } catch (error) {
-      return false
-    }
-  })
-
-  console.log(liked)
-  useEffect(function () {
-    Promise.resolve(
-
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    )
-      .then(() => {
-        const observer = new window.IntersectionObserver(function (entires) {
-          const { isIntersecting } = entires[0]
-          if (isIntersecting) {
-            setShow(true)
-            observer.disconnect()
-          }
-        })
-        observer.observe(element.current)
-      })
-  }, [element])
-
+  const [liked, setLiked] = useLocalStorage(key, false)
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, value)
-      setLiked(value)
-    } catch (e){
-      console.error(e)
-    }
-  }
   return (
     <Article ref={element}>
       {
@@ -56,7 +21,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLocalStorage(!liked)}>
+          <Button onClick={() => setLiked(!liked)}>
             <Icon size='32px' />{likes} likes!
           </Button>
         </Fragment>
