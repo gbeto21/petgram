077077cd313e00603216f99a3aa29dcc22b5fad7
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-21 19:07:47 -0600

    Se creo la pagina para acceder al home. Se instalo la ruta del home con el Reach Router. Se termino la clase: 'Que es Reach Router? Creando la ruta Home'.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-/* eslint-disable react/jsx-closing-tag-location */
-/* eslint-disable react/jsx-fragments */
-import React, { Fragment } from 'react'
-import { ListOfCategories } from './components/ListOfCategories'
+import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
-import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+import { Home } from './pages/Home'
+import { Router } from '@reach/router'
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search)
@@ -18,10 +16,10 @@ export const App = () => {
       {
         detailId
           ? <PhotoCardWithQuery id={detailId} />
-          : <Fragment>
-            <ListOfCategories />
-            <ListOfPhotoCards categoryId={1} />
-          </Fragment>
+          : <Router>
+            <Home path='/' />
+            <Home path='/pet/:id' />
+          </Router> 
       }
     </div>
   )
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-fragments */
+import React, { Fragment } from 'react'
+import { ListOfCategories } from '../components/ListOfCategories'
+import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
+
+export const Home = ({ id }) => {
+  return (
+    <Fragment>
+      <ListOfCategories />
+      <ListOfPhotoCards categoryId={id} />
+    </Fragment>
+  )
+}
