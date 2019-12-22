49fb754e654046e329670ed1e91c9ce5dd3d2bb9
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-21 19:38:39 -0600

    Se creo la pagina del detalle del elemento seleccionado y poder permitir navegar hacia el. Se termino la clase: 'Creando la pagina Detail'.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react'
-import { GlobalStyle } from './styles/GlobalStyles'
-import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
-import { Home } from './pages/Home'
-import { Router } from '@reach/router'
+import React from "react";
+import { GlobalStyle } from "./styles/GlobalStyles";
+import { Logo } from "./components/Logo";
+import { Home } from "./pages/Home";
+import { Router } from "@reach/router";
+import { Detail } from "./pages/Detail";
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
   return (
     <div>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router> 
-      }
+      <Router>
+        <Home path="/" />
+        <Home path="/pet/:id" />
+        <Detail path="/detail/:detailId" />
+      </Router>
     </div>
-  )
-}
+  );
+};
