1fd54f1a9ce8b4f189cda3360509dc799bef0f43
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-22 18:34:43 -0600

    Se creo el layout contenedor de la metadata. Se termino la clase: React Helmet.'.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable react/jsx-fragments */
-import React, { Fragment } from 'react'
-import { ListOfCategories } from '../components/ListOfCategories'
-import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
+import React from "react";
+import { ListOfCategories } from "../components/ListOfCategories";
+import { ListOfPhotoCards } from "../container/ListOfPhotoCards";
+import { Helmet } from "react-helmet";
+import { Layout } from "../components/Layout";
 
 export const Home = ({ id }) => {
   return (
-    <Fragment>
+    <Layout
+      title="Tu app de fotos de mascotas"
+      subtitle="Con Petgram puedes encontrar fotos de animales domesticos muy bonitos."
+    >
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
-    </Fragment>
-  )
-}
+    </Layout>
+  );
+};
