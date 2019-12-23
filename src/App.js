0d7783848057a823b3f1b4137bd330e077f78053
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-22 19:08:18 -0600

    Se agrego la funcionalidad de cargar solamente la pagina de Favs cuando se quiere acceder a ella. Se termino la clase: 'React.lazy() y componente Suspense'.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, Suspense } from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo";
 import { Home } from "./pages/Home";
 import { Router, Redirect } from "@reach/router";
 import { Detail } from "./pages/Detail";
-import { Favs } from "./pages/Favs";
+// import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 import { NavBar } from "./components/NavBar";
 import { Context } from "./Context";
 import { NotFound } from "./pages/NotFound";
 
+const Favs = React.lazy(() => import("./pages/Favs"));
+
 export const App = () => {
   const { isAuth } = useContext(Context);
 
   return (
-    <div>
+    <Suspense fallback={<div />}>
       <GlobalStyle />
       <Logo />
       <Router>
@@ -31,6 +33,6 @@ export const App = () => {
         <User path="/user" />
       </Router>
       <NavBar />
-    </div>
+    </Suspense >
   );
 };
--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { FavsWithQuery } from "../container/GetFavorites";
-import { Helmet } from "react-helmet";
 import { Layout } from "../components/Layout";
 
-export const Favs = () => (
+export default () => (
   <Layout title="Tus favoritos" subtitle="Aqui puedes encontrar tus favoritos.">
     <FavsWithQuery />
   </Layout>
