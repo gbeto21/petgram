9c3e358720099b162a0d785e15885d78672dc027
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-22 23:26:53 -0600

    Se creo el archivo manifest para poder crear la aplicacion PWA. Se termino la clase: 'PWA: generando el manifes'.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaMainifestPlugin = require('webpack-pwa-manifest');
+const path = require('path')
 
 module.exports = {
   output: {
@@ -6,11 +8,23 @@ module.exports = {
     publicPath: '/'
   },
   plugins: [
-    new HtmlWebpackPlugin(
-      {
-        template: 'src/index.html'
-      }
-    )],
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new WebpackPwaMainifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      shorname: 'Petgram',
+      description: 'Con petgram puedes encontrar fotos de animales domesticos muy facilmente',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    })
+  ],
   module: {
     rules: [
       {
@@ -26,4 +40,4 @@ module.exports = {
       }
     ]
   }
-}
+};
